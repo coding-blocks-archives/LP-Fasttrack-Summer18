4c8a45504fc3060997f2a71f1cf51030bc17bb59
Deepak Aggarwal <daggarwal77@gmail.com>
2018-06-23 05:34:27 +0100

    Tree-creation

---
+++ 17-trees/01-intro.cc
@@ -0,0 +1,34 @@
+// Deepak Aggarwal, Coding Blocks
+// deepak@codingblocks.com
+#include <iostream>
+using namespace std;
+
+class TreeNode{
+public:
+    int data;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int d){
+        data = d;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+TreeNode* createTree(){
+    int x; cin >> x;
+    if (x == -1) return NULL;
+
+    TreeNode* root = new TreeNode(x);
+    cout << "Enter left child of " << x << " ";
+    root->left = createTree();
+    cout << "Enter right child of " << x << " ";
+    root->right = createTree();
+    return root;
+}
+
+
+int main(){
+    TreeNode* root = createTree();
+    
+}
