38bc7e5359b689c44c238bab081d1e762bafa5a0
Deepak Aggarwal <daggarwal77@gmail.com>
2018-06-23 12:28:17 +0530

    Trees: Diameter

--- 17-trees/01-intro.cc
+++ 17-trees/01-intro.cc
@@ -20,15 +20,45 @@ TreeNode* createTree(){
     if (x == -1) return NULL;
 
     TreeNode* root = new TreeNode(x);
-    cout << "Enter left child of " << x << " ";
+    // cout << "Enter left child of " << x << " ";
     root->left = createTree();
-    cout << "Enter right child of " << x << " ";
+    // cout << "Enter right child of " << x << " ";
     root->right = createTree();
     return root;
 }
 
+void inorder(const TreeNode* root){
+    if (root == NULL){
+        return;
+    }
+    inorder(root->left);
+    cout << root->data << " ";
+    inorder(root->right);
+}
+
+// TODO Do inorder traversal iteratively
+
+int height(const TreeNode* root){
+    if (root == NULL) return 0;
+    int x = height(root->left);
+    int y = height(root->right);
+    return max(x, y) + 1;
+}
+
+int diameter(const TreeNode* root){
+    if (root == NULL){
+        return 0;
+    }
+    int left_dia = diameter(root->left);
+    int right_dia = diameter(root->right);
+    int curr_dia = height(root->left) + height(root->right) + 1;
+    return max(max(left_dia, right_dia), curr_dia);
+}
 
 int main(){
     TreeNode* root = createTree();
+    inorder(root); cout << endl;
+    int ans = diameter(root);
+    cout << ans;
     
 }
